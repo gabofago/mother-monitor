b387dc9366893267318a0d356e09bb7d816e3ea4
gabofago <gabo.faggot@cock.li>
2016-03-25 21:45:06 -0400

    Create mummySweety.js

---
+++ mummySweety.js
@@ -0,0 +1,21 @@
+var request = require("request");
+
+var dataFromMummy = null;
+request("http://mother-mont.herokuapp.com/archive", function(err, resp, body){
+    if(!err && resp.statusCode == 200){
+        try{
+            dataFromMummy = JSON.parse(body);
+        }catch(e){
+            console.log(e);
+        }
+
+        console.log("posts in archive: " + dataFromMummy.length);
+
+        //go through the list of archives
+        for(var i = 0; i < dataFromMummy.length; i++){
+            for(postId in dataFromMummy[i]){
+                console.log("post id "+ postId + ", " + dataFromMummy[i][postId]['semantic_url']);
+            }
+        }
+    }
+});
